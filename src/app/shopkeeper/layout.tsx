e831887ca750583492a85128af1b7ccdbd5dfeb3
Gorilla Facts <inquiry.gorillafacts@gmail.com>
2025-10-02 05:14:08 +0000

    'use client';
    import Link from 'next/link';
    import { usePathname } from

--- src/app/shopkeeper/layout.tsx
+++ src/app/shopkeeper/layout.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import { LayoutDashboard, User, Bot, LifeBuoy, Users, PieChart } from 'lucide-react';
+import { LayoutDashboard, User, LifeBuoy, Users, PieChart, Bot } from 'lucide-react';
 import Link from 'next/link';
 import { usePathname, useRouter } from 'next/navigation';
 import { useFirebase } from '@/firebase/client-provider';
