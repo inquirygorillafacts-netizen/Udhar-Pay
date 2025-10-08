3f7f3c4390bdb914db84bde14714c53fd8c1231d
Gorilla Facts <inquiry.gorillafacts@gmail.com>
2025-10-08 06:17:07 +0000

    यार अभी भी ट्रांसपेनएट है ये मेसज जो आते है कुछ अरर पर कुछ टाइम के लिए स

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -31,7 +31,7 @@ const toastVariants = cva(
       variant: {
         default: "border bg-white text-black",
         destructive:
-          "destructive group border-destructive bg-destructive text-destructive-foreground",
+          "border bg-white text-black",
       },
     },
     defaultVariants: {
