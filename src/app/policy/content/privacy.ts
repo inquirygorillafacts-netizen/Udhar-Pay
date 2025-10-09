a18f5d1e6000811118fc858abb58e7af95fcb236
Gorilla Facts <inquiry.gorillafacts@gmail.com>
2025-10-09 10:30:00 +0000

    Privacy Policy Last updated on March 19th, 2024
    1. Introduction to Priv

--- src/app/policy/content/privacy.ts
+++ src/app/policy/content/privacy.ts
@@ -1,27 +1,62 @@
 export const privacyContent = `
-<h2>1. Information We Collect</h2>
-<p>We collect information you provide directly to us, such as when you create an account, and information that is automatically collected when you use our services.</p>
-<ul>
-    <li><strong>Personal Information:</strong> Name, email address, phone number, and profile picture.</li>
-    <li><strong>Transaction Information:</strong> Details of credit and payment transactions you make, including amounts, dates, and parties involved.</li>
-    <li><strong>Usage Information:</strong> We collect information about how you use our app, such as the features you use and the time you spend on the app.</li>
-</ul>
+<h2>Privacy Policy</h2>
+<p>Last updated on March 19th, 2024</p>
 
-<h2>2. How We Use Your Information</h2>
-<p>We use the information we collect to:</p>
-<ul>
-    <li>Provide, maintain, and improve our services.</li>
-    <li>Process transactions and send you related information.</li>
-    <li>Communicate with you about products, services, offers, and events.</li>
-    <li>Monitor and analyze trends, usage, and activities in connection with our services.</li>
-</ul>
+<h3>1. Introduction to Privacy Policy</h3>
+<p>This Privacy Policy ("Privacy Policy") applies to your use of the Udhar Pay application (e.g., Android and iOS) (collectively, the "Udhar Pay Platform" or "App"). However, it does not apply to any third-party websites linked to the App or to any relationships you may have with businesses listed on Udhar Pay.</p>
+<p>The terms "we," "our," and "us" refer to Udhar Pay, and the terms "you," "your," and "User" refer to you as a user of Udhar Pay. The term "Personal Information" means information that you provide to us that personally identifies you, such as your name, phone number, email address, and any other data linked to such information.</p>
+<p>Our practices and procedures regarding the collection and use of Personal Information are outlined below to ensure the safe use of the App. We have implemented reasonable security practices and procedures appropriate to the nature of the information and our business. While we strive to provide security that exceeds industry standards, due to the inherent vulnerabilities of the internet, we cannot guarantee complete security of all information transmitted to us by you.</p>
+<p>By visiting or using the App, you agree to be bound by this Privacy Policy and consent to the collection, use, processing, and sharing of your Personal Information as described below. If you do not agree with these terms, please do not use the App. Please note that the separately provided Terms and Conditions form an integral part of your use of this App and should be read in conjunction with this Privacy Policy.</p>
 
-<h2>3. Information Sharing</h2>
-<p>We do not share your personal information with third parties except as described in this Privacy Policy. We may share your information with vendors, consultants, and other service providers who need access to such information to carry out work on our behalf.</p>
+<h3>2. Information we collect and how we use it</h3>
+<p>We collect and store your Personal Information. If you provide your third-party account credentials ("Third Party Account Information") to us, you understand that, upon your authorization, certain content and information from those accounts may be transmitted to your Udhar Pay account. Any Third Party Account Information transmitted to us will be covered by this Privacy Policy. You may choose not to provide certain information; however, this may prevent you from registering with us or using some of our features and services.</p>
+<p>We use commercially reasonable efforts to limit the collection of Personal Information to what is necessary to fulfill the purposes identified below. If we use or plan to use your information for a purpose different from the one for which it was collected, we will request your consent beforehand.</p>
+<p>The Personal Information we collect will be used solely to enable you to use the services provided by us, promote a safe and secure experience, assess user interest in our products and services, inform you about online offers and updates, troubleshoot issues, customize your experience, detect and prevent errors, fraud, and other criminal activity, process payments, enforce our terms and conditions, and fulfill any other purpose disclosed to you at the time of collection.</p>
 
-<h2>4. Data Security</h2>
-<p>We take reasonable measures to help protect information about you from loss, theft, misuse, and unauthorized access, disclosure, alteration, and destruction.</p>
+<h3>3. Account information of Merchants (Shopkeepers)</h3>
+<p>If you create an account to take advantage of the full range of services offered on Udhar Pay, we ask for and record Personal Information such as your name, email address, and mobile number. We may collect and store your Sensitive Personal Data or Information—such as financial information, including Know Your Customer ("KYC") documents as required by RBI regulations—as well as any other applicable information that you choose to save in your Udhar Pay account.</p>
+<p>We use your email address to send you updates, news, and newsletters, and to contact you on behalf of other users. If you do not wish to receive communications unrelated to your use of our services, you can unsubscribe. We also use your mobile number to send you transaction-related SMS alerts based on your preferences. If you do not wish to receive such messages, please contact us.</p>
+<p>Udhar Pay assures you that your Personal Information will not be disclosed publicly or sold to any third party.</p>
+<p>You have the option to erase any information you have provided. If you choose to erase your data, Udhar Pay will delete all your stored information from its servers.</p>
 
-<h2>5. Your Choices</h2>
-<p>You may update, correct, or delete information about you at any time by logging into your account. If you wish to delete your account, please contact us, but note that we may retain certain information as required by law or for legitimate business purposes.</p>
+<h3>4. Customer Information</h3>
+<p>We also store customer information, such as mobile number, card details, and email addresses when payments are made through Udhar Pay checkouts. We share this information with the respective businesses only if the customer consents to sharing it during transactions on Udhar Pay-powered applications. However, Udhar Pay is not liable for any misuse of this information by the businesses or their affiliates with whom the customer has shared their data.</p>
+
+<h3>5. Activity</h3>
+<p>We record information relating to your use of Udhar Pay, such as the searches you undertake, the pages you view, your browser type, IP address, location, requested URL, referring URL, and timestamp information. We use this information to administer Udhar Pay and provide the highest possible level of security and service to you. We also use this data in aggregate to perform statistical analyses of user behavior and characteristics, in order to measure interest in and usage of various areas of Udhar Pay. However, you cannot be identified from this aggregated information.</p>
+<p>Our logos, product and service marks and/or names, trademarks, copyrights, and other intellectual property, whether registered or not ("Our IP"), are exclusively owned by us. Without our prior written permission, you agree not to display or use Our IP in any manner.</p>
+
+<h3>6. Cookies</h3>
+<p>We may send cookies to your device to uniquely identify your device and browser and improve the quality of our service. The term "cookies" refers to small pieces of information that a website sends to your device while you are viewing the site. If you choose to disable cookies, some areas of Udhar Pay may not work properly or at all.</p>
+<p>Udhar Pay may use third-party tools that may collect anonymous information about your visits to Udhar Pay. No Personal Information is collected or used in this process. Udhar Pay users can opt out of sharing this information with third parties by disabling or blocking cookies in their browser.</p>
+
+<h3>7. Enforcement</h3>
+<p>We may use the information we collect, in connection with your use of Udhar Pay (including your Personal Information), to investigate, enforce, and apply our Terms and Conditions and Privacy Policy.</p>
+
+<h3>8. Transfer of information</h3>
+<p>We do not share your Personal Information with any third party, except for financial institutions such as banks, the RBI, or other regulatory agencies, as required. We also share your information to provide you with services offered through Udhar Pay, conduct quality assurance testing, facilitate account creation, provide technical and customer support, or deliver specific services at your instruction. These third parties are required to use your Personal Information only to provide the services you request.</p>
+<p>We may share your Personal Information with our parent company, subsidiaries, joint ventures, or other companies under common control. If another company acquires our company or its assets, that company will possess your Personal Information and assume the rights and obligations concerning that information as described in this Privacy Policy.</p>
+<p>We may disclose your Personal Information to third parties when we have a good faith belief that such disclosure is reasonably necessary to: (a) take action regarding suspected illegal activities; (b) enforce or apply our Terms and Conditions and Privacy Policy; (c) comply with legal processes such as search warrants, subpoenas, statutes, or court orders; or (d) protect our rights, reputation, and property, or those of our users, Affiliates, or the public.</p>
+<p>We may disclose aggregate information relating to user behavior to third parties in connection with actual or prospective business relationships, such as advertisers and content distributors.</p>
+
+<h3>9. Links</h3>
+<p>References on this App to any names, marks, products, or services of third parties, or hyperlinks to third-party websites or information, are provided solely for your convenience. They do not constitute or imply our endorsement, sponsorship, or recommendation of any third party. We are not responsible for their privacy practices. We recommend that you read the privacy policies of all such third-party websites.</p>
+
+<h3>10. User access of Personal Information</h3>
+<p>As a registered Udhar Pay user, you can modify certain Personal Information and adjust your privacy preferences by accessing the "Profile" section of this App.</p>
+
+<h3>11. Security</h3>
+<p>Your account is protected. We use industry-standard measures to protect the Personal Information stored in our database. We limit access to your Personal Information to employees and contractors who need it to perform their job functions. If you have any questions about security on Udhar Pay, please contact us at inquiry.yogendrayogi@gmail.com.</p>
+<p>You acknowledge that Udhar Pay is not responsible for any information intercepted during transmission over the Internet, and you hereby release us from any and all claims arising out of or related to the unauthorized use of such intercepted information.</p>
+
+<h3>12. Terms and modifications to this Privacy Policy</h3>
+<p>Our Privacy Policy is subject to change at any time without prior notice. To ensure you are aware of any updates, please review this policy periodically. Any changes will take effect immediately for all Udhar Pay users.</p>
+<p>If you do not agree to any changes in the way we use your Personal Information, you must promptly notify us of your desire to deactivate your account. Continued use of Udhar Pay after any changes to this Privacy Policy will signify your acknowledgment of those changes and your agreement to be bound by their terms.</p>
+
+<h3>13. Applicable law</h3>
+<p>Your use of this App will be governed by and construed in accordance with the laws of India. Users agree that any legal action or proceedings arising from their use may be brought exclusively in the competent courts or tribunals having jurisdiction in Bengaluru, India, and irrevocably submit to the jurisdiction of such courts or tribunals.</p>
+
+<h3>14. Complaints and Grievance Redressal</h3>
+<p>If you contact us to provide feedback, register a complaint, or ask a question, we will record any Personal Information and other content you provide in your communication so that we can effectively respond.</p>
+<p>Any complaints or concerns regarding your Personal Information should be immediately reported in writing to the Grievance Officer at inquiry.yogendrayogi@gmail.com.</p>
 `;
